4e9e9c45b6e05df61d82403d87a9e85def79f90c
Martin Nevlaha <martin.nevlaha@aomvsr.sk>
2024-01-22 13:53:58 +0100

    add node_env

--- src/config-schema/config.schema.ts
+++ src/config-schema/config.schema.ts
@@ -1,6 +1,7 @@
 import * as Joi from '@hapi/joi';
 
 export const configValidationSchema = Joi.object({
+  NODE_ENV: Joi.string().optional().default('development'),
   APP_PORT: Joi.string().required(),
   DB_HOST: Joi.string().required(),
   DB_PORT: Joi.number().default(5432).required(),
--- src/main.ts
+++ src/main.ts
@@ -9,17 +9,19 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService: ConfigService = app.get(ConfigService);
   app.setGlobalPrefix('/api/v1/');
-  const config = new DocumentBuilder()
-    .setTitle('Todo list')
-    .setDescription('REST Api for todo list')
-    .setVersion('1.0')
-    .addBearerAuth(
-      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT', in: 'header' },
-      'jwt-token',
-    )
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  if (configService.get('NODE_ENV') !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Todo list')
+      .setDescription('REST Api for todo list')
+      .setVersion('1.0')
+      .addBearerAuth(
+        { type: 'http', scheme: 'bearer', bearerFormat: 'JWT', in: 'header' },
+        'jwt-token',
+      )
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(configService.get('APP_PORT'));
